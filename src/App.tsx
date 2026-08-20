e23f9ea21c9c7c4379b61bc9a0d9263f4f18ed12
davidsolano99 <david.solano0813@gmail.com>
2026-08-20 00:38:10 -0600

    Build data-driven featured projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import './App.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import FeaturedProjects from './components/FeaturedProjects'
 
 function App() {
 
   return (
     <>
       <Header />
-
       <main>
         <Hero />
+        <FeaturedProjects />        
       </main>
     </>
   )
